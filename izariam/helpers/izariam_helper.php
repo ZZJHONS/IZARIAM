711da33725b05847b33904102c6f785fae352591
ZZJHONS <zzjhons@gmail.com>
2012-02-25 11:15:48 +0100

    Upload

---
+++ izariam/helpers/izariam_helper.php
@@ -0,0 +1,196 @@
+<?php
+/*
+ * Project: iZariam
+ * Edited: 12/02/2012
+ * By: ZZJHONS
+ * Info: zzjhons@gmail.com
+ */
+/**
+ * Format the time
+ * @param <int> $seconds
+ * @return <string>
+ */
+function format_time($seconds)
+{
+    $CI =& get_instance();
+    
+    $days = floor($seconds/86400);
+    $hours = floor(($seconds-($days*86400))/3600);
+    $minutes = floor(($seconds-($days*86400)-($hours*3600))/60);
+    $seconds = floor($seconds-($days*86400)-($hours*3600)-($minutes*60));
+    $return = '';
+    $times_count = 0;
+    if ($days > 0 and $times_count < 2)
+    {
+        $times_count++;
+        $return .= $days.$CI->lang->line('d_mini').' ';
+    }
+    if ($hours > 0 and $times_count < 2)
+    {
+        $times_count++;
+        $return .= $hours.$CI->lang->line('h_mini').' ';
+    }
+    if ($minutes > 0 and $times_count < 2)
+    {
+        $times_count++;
+        $return .= $minutes.$CI->lang->line('m_mini').' ';
+    }
+    if ($seconds > 0 and $times_count < 2)
+    {
+        $times_count++;
+        $return .= $seconds.$CI->lang->line('s_mini').' ';
+    }
+    
+    return $return;
+}
+
+/**
+ * Format the time large
+ * @param <int> $seconds
+ * @return <string>
+ */
+function format_time_large($seconds)
+{
+    $CI =& get_instance();
+    
+    $days = floor($seconds/86400);
+    $hours = floor(($seconds-($days*86400))/3600);
+    $minutes = floor(($seconds-($days*86400)-($hours*3600))/60);
+    $seconds = floor($seconds-($days*86400)-($hours*3600)-($minutes*60));
+    $return = '';
+    $times_count = 0;
+    if ($days > 0 and $times_count < 2)
+    {
+        $times_count++;
+        if ($days <= 1) {
+            $return .= $days.' '.$CI->lang->line('day');
+        } else {
+            $return .= $days.' '.$CI->lang->line('days');
+        }
+    }
+    if ($hours > 0 and $times_count < 2)
+    {
+        $times_count++;
+        if ($hours <= 1) {
+            $return .= $hours.' '.$CI->lang->line('hour');
+        } else {
+            $return .= $hours.' '.$CI->lang->line('hours');
+        }
+    }
+    if ($minutes > 0 and $times_count < 2)
+    {
+        $times_count++;
+        if ($minutes <= 1) {
+            $return .= $minutes.' '.$CI->lang->line('minute');
+        } else {
+            $return .= $minutes.' '.$CI->lang->line('minutes');
+        }
+    }
+    if ($seconds > 0 and $times_count < 2)
+    {
+        $times_count++;
+        if ($seconds <= 1) {
+            $return .= $seconds.' '.$CI->lang->line('second');
+        } else {
+            $return .= $seconds.' '.$CI->lang->line('seconds');
+        }
+    }
+    return $return;
+}
+
+function premium_time($seconds)
+{
+    $CI =& get_instance();
+
+    $days = floor($seconds/86400);
+    $hours = floor(($seconds-($days*86400))/3600);
+    $minutes = floor(($seconds-($days*86400)-($hours*3600))/60);
+    $seconds = floor($seconds-($days*86400)-($hours*3600)-($minutes*60));
+    $return = ($days > 0) ? $days.$CI->lang->line('d_mini').' ' : '';
+    if ($days > 0)
+    {
+        $return = $days.$CI->lang->line('d_mini');
+    }
+    elseif($days == 0 and $hours > 0)
+    {
+        $return = $hours.$CI->lang->line('h_mini');
+    }
+    elseif($days == 0 and $hours == 0 and $minutes > 0)
+    {
+        $return = $minutes.$CI->lang->line('m_mini');
+    }
+    elseif($days == 0 and $hours == 0 and $minutes == 0 and $seconds > 0)
+    {
+        $return = $seconds.$CI->lang->line('s_mini');
+    }
+    else
+    {
+        $return = '';
+    }
+    return $return;
+}
+
+function route_time($seconds, $hour)
+{
+    $year = floor(date('Y', $seconds));
+    $day = floor(date('d', $seconds));
+    $month = floor(date('m', $seconds));
+    $return = mktime($hour, 0, 0, $month, $day, $year);
+    if ($return < $seconds){$return = mktime($hour, 0, 0, $month, $day+1, $year);}
+    return $return;
+}
+
+/**
+ * Generating a Key
+ * @param <int> $length
+ * @return <string>
+ */
+function random_key($length = 0)
+{
+    $arr = array('a','b','c','d','e','f',
+                 'g','h','i','j','k','l',
+                 'm','n','o','p','r','s',
+                 't','u','v','x','y','z',
+                 'A','B','C','D','E','F',
+                 'G','H','I','J','K','L',
+                 'M','N','O','P','R','S',
+                 'T','U','V','X','Y','Z',
+                 '1','2','3','4','5','6',
+                 '7','8','9','0');
+    $pass = "";
+    if ($length > 0)
+    for($i = 0; $i < $length; $i++)
+    {
+      $index = rand(0, count($arr) - 1);
+      $pass .= $arr[$index];
+    }
+    return $pass;
+}
+
+function resource_icon($type)
+{
+    switch($type)
+    {
+        case 1: return 'wine'; break;
+        case 2: return 'marble'; break;
+        case 3: return 'glass'; break;
+        case 4: return 'sulfur'; break;
+        default: return 'wood'; break;
+    }
+}
+
+function spy_mission_icon($type)
+{
+    switch($type)
+    {
+        case 1: return 'arrived'; break;
+        case 2: return 'return'; break;
+        case 3: return 'money'; break;
+        case 4: return 'money'; break;
+        case 5: return 'research'; break;
+        case 6: return 'online'; break;
+        case 7: return 'garrison'; break;
+        case 8: return 'fleet'; break;
+        case 9: return 'message'; break;
+    }
+}
